f7b24434a661f50c6556fd6cd5c9a7734b56ee77
Sara Schultz <saschultz95@gmail.com>
2017-03-31 11:09:15 -0700

    add if statement to pass third spec so the program can recognize numbers divisible by three.

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,12 @@ var countNumber = function(number) {
   for(var i=1; i<=number; i++) {
     var resultString = result =+ i;
     $("#result").prepend("<li>" + resultString + "</li>");
+    console.log(resultString);
+    if (resultString % 3 === 0) {
+      alert("There is a number divisible by three!")
+    } else {
+
+    }
   };
 };
 
