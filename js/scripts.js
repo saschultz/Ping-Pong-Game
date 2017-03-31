c26f2aee7addd1eb6cd706f4b3cdfdb829cb399b
Sara Schultz <saschultz95@gmail.com>
2017-03-31 10:46:20 -0700

    add logic in scripts.js to pass second spec of returning range of input number to user.

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,18 @@
 //Back-End Logic:
-var result = 0;
 var countNumber = function(number) {
   for(var i=1; i<=number; i++) {
-    result =+ i;
-    alert(result)
+    var resultString = result =+ i;
+    $("#result").prepend("<li>" + resultString + "</li>");
   };
-  return result;
-  };
-  // var output = output.forEach(function(number) {
-  //   return number;
-  // });
+};
 
 
 //Front-End Logic:
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
     var userInput = parseInt($("#userNumber").val());
-    console.log(userInput);
-    output = countNumber(userInput);
-    $("#result").prepend("<li>" + output + "</li>");
+    var output = countNumber(userInput);
+    // $("#result").prepend("<li>" + output + "</li>");
     event.preventDefault();
   })
 });
