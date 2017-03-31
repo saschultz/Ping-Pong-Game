557529158b1373e486f844da5ce9bab0e50c9f80
Sara Schultz <saschultz95@gmail.com>
2017-03-31 08:36:58 -0700

    add functionality in scripts.js to retrieve wq q exit .exit

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,10 @@
 
 
 //Front-End Logic:
+$(document).ready(function() {
+  $("#formOne").submit(function(event) {
+    var userInput = $("#userNumber").val();
+    console.log(userInput);
+    event.preventDefault();
+  })
+});
