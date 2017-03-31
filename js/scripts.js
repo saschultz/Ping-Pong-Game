025083f95efe2e0531bc710ef45150d66ea05de0
Sara Schultz <saschultz95@gmail.com>
2017-03-31 14:37:35 -0700

    change variable names to be more descriptive.

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,28 @@
 //Back-End Logic:
-var countNumber = function(number) {
+var numberToRange = function(number) {
   for(var i=1; i<=number; i++) {
-    var resultString = (result =+ i);
-    var resultArray = [resultString];
-    resultArray.forEach(function(number) {
-      if (resultArray % 15 === 0) {
-        resultArray.splice(0, 1, "Ping-Pong");
-      } else if (resultArray % 3 === 0) {
-        resultArray.splice(0, 1, "Ping");
-      } else if (resultArray % 5 === 0) {
-        resultArray.splice(0, 1, "Pong");
+    var rangeString = (result =+ i);
+    var rangeArray = [rangeString];
+    rangeArray.forEach(function(number) {
+      if (rangeArray % 15 === 0) {
+        rangeArray.splice(0, 1, "Ping-Pong");
+      } else if (rangeArray % 3 === 0) {
+        rangeArray.splice(0, 1, "Ping");
+      } else if (rangeArray % 5 === 0) {
+        rangeArray.splice(0, 1, "Pong");
       } else {
       }
     });
-    $("#result").prepend("<li>" + resultArray + "</li>");
+    $("#result").prepend("<li>" + rangeArray + "</li>");
   };
 };
-
-
 
 //Front-End Logic:
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
     var userInput = parseInt($("#userNumber").val());
-    var output = countNumber(userInput);
+    var output = numberToRange(userInput);
     // $("#result").prepend("<li>" + resultArray + "</li>");
     event.preventDefault();
   });
-  // location.reload();
 });
