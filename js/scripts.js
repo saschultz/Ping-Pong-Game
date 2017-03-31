1ad785db23321d97c497ef8013f4ea2b67c6900d
Sara Schultz <saschultz95@gmail.com>
2017-03-31 09:47:51 -0700

    add function to count from 1 to input number in alerts.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,24 @@
 //Back-End Logic:
-// const
+var result = 0;
+var countNumber = function(number) {
+  for(var i=1; i<=number; i++) {
+    result =+ i;
+    alert(result)
+  };
+  return result;
+  };
+  // var output = output.forEach(function(number) {
+  //   return number;
+  // });
+
 
 //Front-End Logic:
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
     var userInput = parseInt($("#userNumber").val());
     console.log(userInput);
-    $("#result").prepend("<li>" + userInput + "</li>");
+    output = countNumber(userInput);
+    $("#result").prepend("<li>" + output + "</li>");
     event.preventDefault();
   })
 });
