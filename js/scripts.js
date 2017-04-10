60b9b3168a73794e6aa031ef97f905b925ce09c2
Sara Schultz <saschultz95@gmail.com>
2017-04-09 17:56:04 -0700

    remove unnecessary return after for loop and correct indentation in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,18 @@
 //Back-End Logic:
 var finalOutput = [];
 var numberToRange = function(range) {
-
   var range;
   for(var i=1; i<=range; i++) {
-      if ((i % 15) === 0) {
-        finalOutput.push("Ping-Pong");
-      } else if ((i % 3) === 0) {
-        finalOutput.push("Ping");
-      } else if ((i % 5) === 0) {
-        finalOutput.push("Pong");
-      } else {
-        finalOutput.push(i);
-      }
-    };
-  return finalOutput;
+    if ((i % 15) === 0) {
+      finalOutput.push("Ping-Pong");
+    } else if ((i % 3) === 0) {
+      finalOutput.push("Ping");
+    } else if ((i % 5) === 0) {
+      finalOutput.push("Pong");
+    } else {
+      finalOutput.push(i);
+    }
+  };
 };
 //Front-End Logic:
 $(document).ready(function() {
